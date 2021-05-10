10a3c09a5f9f55a23e7210516ba1eaf0773335fc
Lawrence-Tang <tangzongsheng@gmail.com>
2021-05-10 13:39:13 +0800

    fixed issue that occur when mkfs calculate rootfs size (batch updates)

--- test/test-big-rootfs-img.sh
+++ test/test-big-rootfs-img.sh
@@ -18,9 +18,17 @@ git clone ../../.git sd-fuse_h5
 cd sd-fuse_h5
 wget http://${HTTP_SERVER}/dvdfiles/H5/images-for-eflasher/friendlycore-xenial_4.14_arm64.tgz
 tar xzf friendlycore-xenial_4.14_arm64.tgz
+wget http://${HTTP_SERVER}/dvdfiles/H5/images-for-eflasher/eflasher.tgz
+tar xzf eflasher.tgz
 
-# git clone https://github.com/friendlyarm/linux -b sunxi-4.14.y --depth 1 kernel-h5
-git clone git@192.168.1.5:/allwinner/linux-sunxi.git --depth 1 -b sunxi-4.14.y-devel kernel-h5
+# make big file
+fallocate -l 5G friendlycore-xenial_4.14_arm64/rootfs.img
 
-KERNEL_SRC=$PWD/kernel-h5 ./build-kernel.sh friendlycore-xenial_4.14_arm64
+# calc image size
+IMG_SIZE=`du -s -B 1 friendlycore-xenial_4.14_arm64/rootfs.img | cut -f1`
+
+# re-gen parameter.txt
+./tools/generate-partmap-txt.sh ${IMG_SIZE} friendlycore-xenial_4.14_arm64 h5
+
 sudo ./mk-sd-image.sh friendlycore-xenial_4.14_arm64
+sudo ./mk-emmc-image.sh friendlycore-xenial_4.14_arm64
