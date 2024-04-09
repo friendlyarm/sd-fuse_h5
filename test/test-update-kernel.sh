f73223fb75ab210972397cbb9ea5c49ee6cedbe5
Lawrence-Tang <tangzongsheng@gmail.com>
2024-04-09 17:48:03 +0800

    update test script

--- test/test-update-kernel.sh
+++ test/test-update-kernel.sh
@@ -4,10 +4,7 @@ set -eu
 HTTP_SERVER=112.124.9.243
 
 # hack for me
-PCNAME=`hostname`
-if [ x"${PCNAME}" = x"tzs-i7pc" ]; then
-       HTTP_SERVER=127.0.0.1
-fi
+[ -f /etc/friendlyarm ] && source /etc/friendlyarm $(basename $(builtin cd ..; pwd))
 
 # clean
 mkdir -p tmp
@@ -19,8 +16,6 @@ cd sd-fuse_h5
 wget --no-proxy http://${HTTP_SERVER}/dvdfiles/H5/images-for-eflasher/friendlycore-focal_4.14_arm64.tgz
 tar xzf friendlycore-focal_4.14_arm64.tgz
 
-# git clone https://github.com/friendlyarm/linux -b sunxi-4.14.y --depth 1 kernel-h5
-git clone git@192.168.1.5:/allwinner/linux-sunxi.git --depth 1 -b sunxi-4.14.y-devel kernel-h5
-
+git clone https://github.com/friendlyarm/linux -b sunxi-4.14.y --depth 1 kernel-h5
 KERNEL_SRC=$PWD/kernel-h5 ./build-kernel.sh friendlycore-focal_4.14_arm64
 sudo ./mk-sd-image.sh friendlycore-focal_4.14_arm64
